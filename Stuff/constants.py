156cde3cdafe5390d78789731219fabd2a9a6de5
Stepan Bahnik <bahniks@seznam.cz>
2023-04-03 18:12:36 +0200

    changed to web version

--- Stuff/constants.py
+++ Stuff/constants.py
@@ -4,5 +4,5 @@ CONDITION_LOW = (25, 50, 75)
 CONDITION_HIGH = (100, 200, 300)
 
 TESTING = True
-#URL = "https://bahnik.pythonanywhere.com/"
-URL = "http://127.0.0.1:8000/"
+URL = "https://bahnik.pythonanywhere.com/"
+#URL = "http://127.0.0.1:8000/"
