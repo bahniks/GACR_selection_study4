5eecc1d1f46a74224bb7f100ccd862f78a865201
Stepan Bahnik <bahniks@seznam.cz>
2023-06-03 21:34:16 +0200

    should be prepared for pretest

--- Stuff/constants.py
+++ Stuff/constants.py
@@ -4,7 +4,7 @@ MAX_BDM_PRIZE = 300
 CONDITION_LOW = (25, 50, 75)
 CONDITION_HIGH = (100, 200, 300)
 
-TESTING = True
+TESTING = False
 URL = "https://bahnik.pythonanywhere.com/"
 #URL = "TEST"
 #URL = "http://127.0.0.1:8000/"
